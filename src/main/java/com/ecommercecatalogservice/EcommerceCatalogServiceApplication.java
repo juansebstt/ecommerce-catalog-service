c4da7c361a863ad5e0d469a5d50032e6a1cd4b45
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-08 12:48:06 -0500

    first commit

---
+++ src/main/java/com/ecommercecatalogservice/EcommerceCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceCatalogServiceApplication.class, args);
+    }
+
+}
